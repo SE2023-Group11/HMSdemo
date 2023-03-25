4db9d6338db225776ee97f5ee49531adf2127a67
Gabriel1159 <gabriel1159@163.com>
2023-03-25 15:11:38 +0800

    demo1-finish

--- demo-1/src/components/MyDemo.vue
+++ demo-1/src/components/MyDemo.vue
@@ -1,16 +1,51 @@
 <template>
   <h2>登记患者信息</h2>
-  <el-form :model="ruleForm.name" :rules="rules" ref="ruleForm" label-width="100px">
-    <el-form-item label="活动名称" prop="name">
-      <el-input v-model="ruleForm.name"></el-input>
-    </el-form-item>
-  </el-form>
+  姓名：<input type="text" width="80px" v-model="pname1">
+  <button @click="addPatient">注册</button>
+  <h3>{{ infomation }}</h3>
+  <hr>
+  <h2>查询患者信息</h2>
+  患者ID：<input type="text" width="80px" v-model="pID">
+  <button @click="findNameByID">查询</button>
+  <h3>患者姓名为{{ pname2 }}</h3>
 </template>
 
 <script>
+  import axios from 'axios';
+  import { ref } from 'vue';
   export default {
     name:'MyDemo',
     setup(){
+      let pname1 = ref('')
+      let pname2 = ref('')
+      let pID = ref()
+      let infomation = ref('')
+      function addPatient(){
+        axios.get('http://localhost:8080/addPatient', {params:{
+            pname:pname1.value
+        }}).then(()=>{
+            infomation.value = '注册成功！'
+        }).catch(()=>{
+            infomation.value = '注册失败！'
+        })
+      }
+      function findNameByID(){
+        axios.get('http://localhost:8080/showPatient', {params:{
+            pid:pID.value
+        }}).then(res=>{
+            pname2.value = res.data
+        }).catch(err=>{
+            console.log(err)
+        })
+      }
+      return {
+        pname1, 
+        pname2,
+        pID,
+        infomation,
+        findNameByID,
+        addPatient
+      }
     }
   }
 </script>
--- demo-1/src/main.js
+++ demo-1/src/main.js
@@ -2,14 +2,3 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app')
-
-import { request } from './network/request/request'
-request({
-    url:'getPatients',
-    success:res=>{
-        console.log(res)
-    },
-    fail:err=>{
-        console.log(err)
-    }
-})
